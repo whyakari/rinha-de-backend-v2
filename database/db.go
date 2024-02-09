91f3e45be476bf8102fedaed021cc7ad259aec76
akari <akariondev@gmail.com>
2024-02-09 18:43:06 +0000

    database/db: Switch database connection to MariaDB driver and add connection validation
    Signed-off-by: akari <akariondev@gmail.com>

--- database/db.go
+++ database/db.go
@@ -2,17 +2,24 @@ package db
 
 import (
     "database/sql"
-    _ "github.com/mattn/go-sqlite3"
+    "fmt"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 var DB *sql.DB
 
 func InitDB() error {
     var err error
-    DB, err = sql.Open("sqlite3", "rinha.db")
+    DB, err = sql.Open("mysql", "root:love@tcp(localhost:3306)/rinha")
     if err != nil {
-        return err
+        return fmt.Errorf("erro ao conectar-se ao banco de dados: %v", err)
     }
+ 
+    err = DB.Ping()
+    if err != nil {
+        return fmt.Errorf("erro ao verificar a conexão com o banco de dados: %v", err)
+    }
+    
     return nil
 }
 
