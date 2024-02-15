d7d75b3c4a15f350348c413af18d1a42a812e94a
akari <akariondev@gmail.com>
2024-02-15 16:55:53 +0000

    Update database connection configuration and environment handling
    Signed-off-by: akari <akariondev@gmail.com>

--- database/db.go
+++ database/db.go
@@ -3,23 +3,29 @@ package db
 import (
     "database/sql"
     "fmt"
+    "os"
     _ "github.com/go-sql-driver/mysql"
 )
 
 var DB *sql.DB
 
 func InitDB() error {
+    dbHostname := os.Getenv("DB_HOSTNAME")
+    if dbHostname == "" {
+        return fmt.Errorf("DB_HOSTNAME environment variable is not set")
+    }
+
+    dbConnString := fmt.Sprintf("root:love@tcp(%s:3306)/rinha", dbHostname)
     var err error
-    DB, err = sql.Open("mysql", "root:love@tcp(db:3306)/rinha")
+    DB, err = sql.Open("mysql", dbConnString)
     if err != nil {
         return fmt.Errorf("error connecting to database: %v", err)
     }
- 
+
     err = DB.Ping()
     if err != nil {
         return fmt.Errorf("error checking database connection: %v", err)
     }
- 
+
     return nil
 }
-
--- main.go
+++ main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"strings"
-	"github.com/gin-gonic/gin"
-	_ "github.com/mattn/go-sqlite3"
-	"github.com/whyakari/rinha-de-backend-v2/api/handlers"
-	db "github.com/whyakari/rinha-de-backend-v2/database"
+    "fmt"
+    "log"
+    "os"
+    "strings"
+    "github.com/gin-gonic/gin"
+    _ "github.com/mattn/go-sqlite3"
+    "github.com/whyakari/rinha-de-backend-v2/api/handlers"
+    db "github.com/whyakari/rinha-de-backend-v2/database"
 )
 
 func main() {
     if err := db.InitDB(); err != nil {
-        log.Fatal("Error initializing the database:", err)
+        log.Fatal("Erro ao inicializar o banco de dados:", err)
     }
 
-	if err := executeSchemaSQL("schema.sql"); err != nil {
-		log.Fatal("Error when executing schema.sql file:", err)
-	}
+    if err := executeSchemaSQL("schema.sql"); err != nil {
+        log.Fatal("Error when executing schema.sql file:", err)
+    }
 
-	router := gin.Default()
-	router.POST("/clientes/:id/transacoes", handlers.HandleTransacoes)
-	router.GET("/clientes/:id/extrato", handlers.HandleExtrato)
+    router := gin.Default()
+    router.POST("/clientes/:id/transacoes", handlers.HandleTransacoes)
+    router.GET("/clientes/:id/extrato", handlers.HandleExtrato)
 
-	if err := router.Run(":3000"); err != nil {
-		log.Fatal("Error starting Gin server:", err)
-	}
+    if err := router.Run(":3000"); err != nil {
+        log.Fatal("Error starting Gin server:", err)
+    }
 }
 
 func executeSchemaSQL(filePath string) error {
