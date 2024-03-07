a337e5636e23616f2e89ccac0fa741a4d9e2aefd
akari <akariondev@gmail.com>
2024-03-07 14:19:07 +0000

    loadBalancer: Add connection limit semaphore to server
    Signed-off-by: akari <akariondev@gmail.com>

--- loadBalancer/main.go
+++ loadBalancer/main.go
@@ -1,31 +1,39 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"log"
-	"net"
-	"net/http"
-	"net/url"
-	"sync/atomic"
+    "fmt"
+    "io"
+    "log"
+    "net"
+    "net/http"
+    "net/url"
+    "sync/atomic"
+)
+
+var (
+    maxConnections = 2000
+    sem            = make(chan struct{}, maxConnections)
 )
 
 type RoundRobin struct {
-	addrs      []string
-	reqCounter uint64
+    addrs      []string
+    reqCounter uint64
 }
 
 func (rr *RoundRobin) NextServer() string {
-	counter := atomic.AddUint64(&rr.reqCounter, 1)
-	return rr.addrs[counter%uint64(len(rr.addrs))]
+    counter := atomic.AddUint64(&rr.reqCounter, 1)
+    return rr.addrs[counter%uint64(len(rr.addrs))]
 }
 
 type ProxyHandler struct {
-	loadBalancer *RoundRobin
-	client       *http.Client
+    loadBalancer *RoundRobin
+    client       *http.Client
 }
 
 func (ph *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    sem <- struct{}{}
+    defer func() { <-sem }()
+
     targetAddr := ph.loadBalancer.NextServer()
     targetURL, _ := url.Parse("http://" + targetAddr)
 
@@ -53,56 +61,58 @@ func (ph *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleHTTPServer() {
-	addrs := []string{"localhost:3000", "localhost:3000"}
-	roundRobin := &RoundRobin{addrs: addrs}
-	client := &http.Client{}
+    addrs := []string{"api01:3000", "api02:3000"}
+    roundRobin := &RoundRobin{addrs: addrs}
+    client := &http.Client{}
 
-	http.Handle("/", &ProxyHandler{loadBalancer: roundRobin, client: client})
+    http.Handle("/", &ProxyHandler{loadBalancer: roundRobin, client: client})
 
-	port := ":9999"
-	fmt.Println("HTTP Proxy server listening on port", port)
-	if err := http.ListenAndServe(port, nil); err != nil {
-		log.Fatal("Error starting HTTP proxy server:", err)
-	}
+    fmt.Println("HTTP Proxy server listening on port :9999")
+    if err := http.ListenAndServe(":9999", nil); err != nil {
+        log.Fatal("Error starting HTTP proxy server:", err)
+    }
 }
 
 func handleTCPConnection() {
-	addrs := []string{"api01:3000", "api02:3000"}
-	roundRobin := &RoundRobin{addrs: addrs}
+    addrs := []string{"api01:3000", "api02:3000"}
+    roundRobin := &RoundRobin{addrs: addrs}
 
-	port := ":9999"
-	tcpListener, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatal("Error starting TCP proxy server:", err)
-	}
-	defer tcpListener.Close()
+    port := ":9999"
+    tcpListener, err := net.Listen("tcp", port)
+    if err != nil {
+        log.Fatal("Error starting TCP proxy server:", err)
+    }
+    defer tcpListener.Close()
 
-	fmt.Println("TCP Proxy server listening on port", port)
+    fmt.Println("TCP Proxy server listening on port", port)
 
-	for {
-		downstream, err := tcpListener.Accept()
-		if err != nil {
-			log.Println("Error accepting TCP connection:", err)
-			continue
-		}
-		go func(downstream net.Conn) {
-			defer downstream.Close()
+    for {
+        downstream, err := tcpListener.Accept()
+        if err != nil {
+            log.Println("Error accepting TCP connection:", err)
+            continue
+        }
+        go func(downstream net.Conn) {
+            defer downstream.Close()
 
-			upstreamAddr := roundRobin.NextServer()
-			upstream, err := net.Dial("tcp", upstreamAddr)
-			if err != nil {
-				log.Println("Error connecting to upstream:", err)
-				return
-			}
-			defer upstream.Close()
+            sem <- struct{}{}
+            defer func() { <-sem }()
 
-			go io.Copy(upstream, downstream)
-			io.Copy(downstream, upstream)
-		}(downstream)
-	}
+            upstreamAddr := roundRobin.NextServer()
+            upstream, err := net.Dial("tcp", upstreamAddr)
+            if err != nil {
+                log.Println("Error connecting to upstream:", err)
+                return
+            }
+            defer upstream.Close()
+
+            go io.Copy(upstream, downstream)
+            io.Copy(downstream, upstream)
+        }(downstream)
+    }
 }
 
 func main() {
-	//handleHTTPServer()
-	handleTCPConnection()
+    //handleHTTPServer()
+    handleTCPConnection()
 }
