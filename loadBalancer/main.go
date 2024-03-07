11cfcb1d325486f69435fe8bc27646eea0d9b0af
Akari <akariondev@gmail.com>
2024-03-07 10:01:08 +0000

    compose fix ports
    Signed-off-by: Akari <akariondev@gmail.com>

--- loadBalancer/main.go
+++ loadBalancer/main.go
@@ -54,7 +54,7 @@ func (ph *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	addrs := []string{"api01:3001", "api02:3002"}
+	addrs := []string{"api01:3000", "api02:3000"}
 
 	roundRobin := &RoundRobin{addrs: addrs}
 
