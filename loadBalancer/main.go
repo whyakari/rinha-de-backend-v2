5886fcdb256de04559678c4297a9e8a60d217bd5
akari <akariondev@gmail.com>
2024-03-07 11:05:56 +0000

    loadBalancer: Implement TCP load balancing feature in Go proxy server With this implementation, the Go proxy server is now capable of handling both TCP and HTTP connections. It listens for TCP connections on port 9999 and redirects them to upstream servers using round robin load balancing algorithm.
    Signed-off-by: akari <akariondev@gmail.com>

--- loadBalancer/main.go
+++ loadBalancer/main.go
@@ -4,32 +4,29 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"net/url"
 	"sync/atomic"
 )
 
-type LoadBalancer interface {
-	NextServer(req *http.Request) string
-}
-
 type RoundRobin struct {
-	addrs       []string
-	reqCounter  uint64
+	addrs      []string
+	reqCounter uint64
 }
 
-func (rr *RoundRobin) NextServer(req *http.Request) string {
+func (rr *RoundRobin) NextServer() string {
 	counter := atomic.AddUint64(&rr.reqCounter, 1)
 	return rr.addrs[counter%uint64(len(rr.addrs))]
 }
 
 type ProxyHandler struct {
-	loadBalancer LoadBalancer
+	loadBalancer *RoundRobin
 	client       *http.Client
 }
 
 func (ph *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	targetAddr := ph.loadBalancer.NextServer(r)
+	targetAddr := ph.loadBalancer.NextServer()
 	targetURL := &url.URL{
 		Scheme: "http",
 		Host:   targetAddr,
@@ -53,17 +50,48 @@ func (ph *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	io.Copy(w, resp.Body)
 }
 
+func handleTCPConnection(listener net.Listener, loadBalancer *RoundRobin) {
+	for {
+		downstream, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting connection:", err)
+			continue
+		}
+		go func(downstream net.Conn) {
+			defer downstream.Close()
+
+			upstreamAddr := loadBalancer.NextServer()
+			upstream, err := net.Dial("tcp", upstreamAddr)
+			if err != nil {
+				log.Println("Error connecting to upstream:", err)
+				return
+			}
+			defer upstream.Close()
+
+			go io.Copy(upstream, downstream)
+			io.Copy(downstream, upstream)
+		}(downstream)
+	}
+}
+
 func main() {
 	addrs := []string{"api01:3000", "api02:3000"}
-
 	roundRobin := &RoundRobin{addrs: addrs}
-
 	client := &http.Client{}
 
 	http.Handle("/", &ProxyHandler{loadBalancer: roundRobin, client: client})
 
 	port := ":9999"
 	fmt.Println("Proxy server listening on port", port)
+
+	tcpListener, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal("Error starting TCP proxy server:", err)
+	}
+	defer tcpListener.Close()
+
+	go handleTCPConnection(tcpListener, roundRobin)
+
 	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatal("Error starting proxy server:", err)
 	}
